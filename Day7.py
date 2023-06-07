75bebff7209af1048ae1a95e369f8f38d448c876
Mr. Muhammad Sultan <134929567+MrSultan22@users.noreply.github.com>
2023-06-07 04:59:52 -0700

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -13,35 +13,36 @@
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Q1. write a program spcrit to sort (ascending and descending order) a dictionary by value?
-# my_Dic = {1:'A', 2:'B', 3:'C', 4:'D', 5:'F'}
-# print(my_Dic)
-# you = my_Dic
-# print(you)
+my_Dic = {4:'A', 2:'B', 5:'C', 1:'D', 3:'F'}
+print("original dictionary: ",my_Dic)
+# it will make ascending order.
+you = list(sorted(my_Dic.items()))
+print("ascending order list: ", you)
+# it will make descending order.
+this = list(sorted(my_Dic.items(), reverse=True))
+print("descending order list: ", this)
 
 # =====================================================================
 # •2 Write a Python script to concatenate three dictionaries to create a
 # new one.
-A = { 1:33, 2:44, 3:55}
+print('=====================================================================')
+A = { 11:33, 22:44, 33:55}
 B = {1:'a', 2:'b'}
-C = {1:'club', 2:'queen', 3:'king', 4:'spade'}
+C = {55:'club', 12:'queen', 23:'king', 34:'spade'}
 # D = (A,B)
 # print(D)
 # concatenate.
 D = {}
-for d in (A,B,C):
+for d in(A,B,C):
     D.update(d)
-    print(D)
-
-
-
-
+print(D)
 
 # =====================================================================
 # Q3. Write a Python script to merge two Python dictionaries.
 print('=====================================================================')
 animals = {10:'elephants', 20:'lions', 15:'Deers'}
 birds   = {3:'parrots', 5:"sparows",2:'ducks'}
-zoo = ("animals ",animals,"Birds",birds)
+zoo = dict(zip(animals,birds))
 print(zoo)
 
 
@@ -49,22 +50,40 @@ print(zoo)
 # •4 Write a Python script to create a dictionary where the keys are
 # numbers between 1 and 15 (both included) and the values are
 # square of keys.
+print('=====================================================================')
 
-
-
-
-
+plants = {}
+for i in range(1,16):
+    plants[i]=i**2
+print(plants)
+print('==========================================')
 # =====================================================================
 # •Q5. Write a Python program to map two lists into a dictionary.
-
-
+#
+# answer is {1: 'A', 2: 'B', 3: 'C', 4: 'D'} because keys are 4 and values are 5 
+# therefore giving this output.
+# letters = ['A','B','C','D','E']
+# numbers = [1,2,3,4]
+#======================
+letters = ['A','B','C','D','E']# consider as values in dictionary.
+numbers = [1,2,3,4,5] # consider as keys in dictionary.
+maping = dict(zip(numbers,letters))
+print('map two lists into a dictionary: ', maping)
+print('==========================================')
 
 
 
 # =====================================================================
 # Q7• Write a Python program to remove duplicates values from
 # Dictionary.
-
+plants = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E'}
+for i in plants:
+    if i in plants:
+        plants.pop(i)
+    else:
+        plants.update(i)
+    # plants[i]=i**2
+print(plants)
 
 
 
